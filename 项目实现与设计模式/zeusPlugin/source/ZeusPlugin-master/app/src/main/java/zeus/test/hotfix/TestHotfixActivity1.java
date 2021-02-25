016eef8d2e4f3c4f6645192e1409bd12549f533f
jianGe <2297369160@qq.com>
2021-02-25 14:28:21 +0800

    Delete 项目实现与设计模式/zeusPlugin directory

--- 项目实现与设计模式/zeusPlugin/source/ZeusPlugin-master/app/src/main/java/zeus/test/hotfix/TestHotfixActivity1.java
+++
@@ -1,7 +0,0 @@
-package zeus.test.hotfix;
-
-/**
- * Created by jimor on 2017/3/15.
- */
-public class TestHotfixActivity1 extends TestHotFixActivity{
-}
--- 项目实现与设计模式/zeusPlugin/source/ZeusPlugin-master/app/src/main/java/zeus/test/hotfixTest/MyInterface.java
+++
@@ -1,9 +0,0 @@
-package zeus.test.hotfixTest;
-
-/**
- * Created by jimor on 2017/3/15.
- */
-public interface MyInterface {
-
-    String getString();
-}
